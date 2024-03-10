8223daacb2f58e1aa7a3370a6a62a1deb4b87b2d
AJ Generoso <ajgeneroso@AJs-MacBook-Air.lan>
2024-03-10 19:28:32 -0400

    updated

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,42 @@
+import { gql } from '@apollo/client';
+
+export const LOGIN_USER = gql`
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+export const ADD_USER = gql`
+  mutation addUser($username: String!, $email: String!, $password: String!) {
+    addUser(username: $username, email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+export const SAVE_BOOK = gql`
+  mutation saveBook($title: String!, $authors: [String], $description: String, $link: String, $image: String, $bookId: Int) {
+    saveBook(title: $title, authors: $authors, description: $description, link: $link, image: $image, bookId: $bookId) {
+      _id
+      bookCount
+    }
+  }
+`;
+
+export const REMOVE_BOOK = gql`
+  mutation removeBook($bookId: String!) {
+    removeBook(bookId: $bookId) {
+      _id
+    }
+  }
+`;
---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,46 @@
+
+const typeDefs = `
+  type User {
+    _id: ID
+    username: String
+    email: String
+    bookCount: Int
+    savedBooks: [Book]
+  }
+
+  type Book {
+    bookId: String!
+    authors: [String]
+    description: String!
+    title: String!
+    link: String
+    image: String
+  }
+
+  type Auth {
+    token: ID!
+    user: User
+  }
+
+  type Query {
+    me: User
+    
+  }
+
+  type Mutation {
+    addUser(username: String!, email: String!, password: String!): Auth
+    login(email: String!, password: String!): Auth
+
+    saveBook(
+      bookId: Int
+      authors: [String]
+      description: String
+      title: String!
+      link: String
+      image: String): User
+
+    removeBook(bookId: String!): User
+  }
+`;
+
+module.exports = typeDefs;
